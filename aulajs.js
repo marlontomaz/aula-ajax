4820e567b518599af598589213231642b716d1e6
Marlon Tomazeli <marlon.tomazeli@gmail.com>
2019-01-09 21:13:44 -0200

    first commit

---
+++ aulajs.js
@@ -0,0 +1,22 @@
+//js 
+
+function grande()
+{
+	var input = document.getElementById("texto");
+	var resultado = document.getElementById("resultado");
+
+	var texto = input.value;
+
+	resultado. innerHTML = texto.toUpperCase();
+}
+
+
+function cidade()
+{
+	var cidade = document.getElementById("cidade");
+	var lista = document.getElementById("lista");
+
+	var novo = '<li>'+ cidade.value +'</li>';
+
+	lista. innerHTML = 	lista. innerHTML + novo;
+}
