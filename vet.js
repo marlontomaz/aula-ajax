8bd2c4111385ec0c19bdfd841b89dea51f667155
Marlon Tomazeli <marlon.tomazeli@gmail.com>
2019-01-10 21:55:15 -0200

    Vetor Ordenar

---
+++ vet.js
@@ -0,0 +1,32 @@
+
+var vetor = new Array();
+
+
+function cadastrar()
+{
+    var input = document.getElementById("texto");
+       
+    vetor.push (input.value);
+    exibir();
+}
+
+function ordenar()
+{
+    vetor.sort();
+    exibir();
+    
+
+}
+
+function exibir()
+
+{
+    var resultado = document.getElementById("resultado");
+
+    resultado.innerHTML ="";
+
+    for (var i = 0; i < vetor.length; i++)
+    {
+       resultado.innerHTML += "<li>" + vetor[i] + "</li>";
+    }
+}
