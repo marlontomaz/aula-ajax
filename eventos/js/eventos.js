8be345559d95671fc3a24abe269a3eb51e566370
Marlon Tomazeli <marlon.tomazeli@gmail.com>
2019-01-22 21:54:45 -0200

    site eventos _ ajax

--- eventos/js/eventos.js
+++ eventos/js/eventos.js
@@ -1,12 +1,22 @@
-//baixa o conteudo da variavel json e cria a variavel dados 
+//baixa o conteudo da variavel json e cria a variavel dados  
 $.getJSON("model/eventos_listar.php", function(dados){
     // para cada item em dados gera um valor
     dados.forEach(function(valor){
+
+        var data = valor.datahora.split(" ");
+        var dia = data[0].split("-");
+        var hora = data[1].split(":");
+
+        
+
+        var dataFormatada = dia[2]+"/"+dia[1]+"/"+dia[0];
+        var horaFormatada = hora[0]+":"+hora[2];
+
         // gerando a linha com os dados 
         var tr = "<tr>"
             +"<td>"+ valor.id +"</td>"
             +"<td>"+ valor.nome +"</td>"
-            +"<td>"+ valor.datahora +"</td>"
+            +"<td>"+ dataFormatada +","+ horaFormatada +"</td>"
             +"<td>"+ valor.local +"</td>"
             +"<td>[Editar] [Deletar]</td>"
             +"</tr>";
